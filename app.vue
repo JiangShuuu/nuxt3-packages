dd764a26de9976e4d0ddc0e1297e60bf2eb467ac
JiangShuuu <globelex65@gmail.com>
2023-06-10 09:24:09 +0800

    first commit

---
+++ app.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="h-[2000px] flex  justify-between">
+    <!-- <NuxtWelcome /> -->
+    <!-- Open the modal using ID.showModal() method -->
+    <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+    <div>dsadsa</div>
+    <dialog id="my_modal_1" class="modal overflow-y-scroll">
+      <form method="dialog" class="w-[1192px] bg-white h-auto rounded p-7 ">
+        <div class="w-full text-end">
+          <button class="text-xl">X</button>
+        </div>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        <p class="text-2xl mt-8">新增專案次分類</p>
+        
+      </form>
+    </dialog>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ['light'],
+  },
+  plugins: [daisyui],
+}
