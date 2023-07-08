a4acd36130ef0ce14eeed9330dccb005e0097ce0
JiangShuuu <globelex65@gmail.com>
2023-07-08 18:02:02 +0800

    init

--- pages/app-auth/index.vue
+++ pages/app-auth/index.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex h-screen flex-col items-center justify-center">
-    <div class="flex items-center space-x-4">
+    <div
+      class="flex h-20 w-[300px] items-center justify-center space-x-4 border"
+    >
       <img :src="user.avatar" class="h-12 w-12 rounded-full" />
       <div class="space-y-2">
-        <p class="h-4 w-[100px]">
+        <p class="w-full">
           {{ user.name }}
         </p>
-        <p class="h-4 w-[250px]">
+        <p class="w-full">
           {{ user.email }}
         </p>
       </div>
