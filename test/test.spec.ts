7bf15b5338af1c3fded8d833e72046f8793613ff
JiangShuuu <globelex65@gmail.com>
2023-12-19 15:54:59 +0800

    add test

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -17,3 +17,9 @@ describe('my component', () => {
     console.log(wrapper.html())
   })
 })
+
+it('1 + 1 should not to be 11', () => {
+  const add = (x: number | string, y: number | string) => Number(x) + Number(y)
+  expect(add(1, 1)).not.toBe('11')
+  expect(add('1', '1')).toBe(2)
+})
